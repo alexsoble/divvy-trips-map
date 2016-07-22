0b9d2706bff55faeff59f75f6abc237d68125b80
Alex Soble <asoble@gmail.com>
2016-07-22 08:09:43 -0500

    Add trip start/end info

--- map.js
+++ map.js
@@ -9,4 +9,37 @@ var myStyle = {
   "opacity": 0.5
 };
 
-L.geoJson(data, { style: myStyle }).addTo(map);
+function highlightFeature(e) {
+  var props = e.target.feature.geometry.properties;
+
+  var infoText = '<b>Start: </b>' + props.startStation +
+                 '<br/><br/><b>End: </b>' + props.endStation +
+                 '<br/><br/><b>Frequency: </b>' + props.totalTrips;
+
+  info.update({ infoText: infoText });
+}
+
+function onEachFeature(feature, layer) {
+  layer.on({
+    mouseover: highlightFeature,
+  });
+}
+
+var info = L.control({ position: 'bottomleft' });
+
+info.onAdd = function (map) {
+  this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+  this.update();
+  return this._div;
+};
+
+info.update = function (props) {
+  this._div.innerHTML = (props ? props.infoText : null);
+};
+
+info.addTo(map);
+
+L.geoJson(data, {
+  style: myStyle,
+  onEachFeature: onEachFeature
+}).addTo(map);
